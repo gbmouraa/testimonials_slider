f65c0489248263951988f0c6c5d39d84d0d8c1ce
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-10-29 20:33:28 -0300

    adicionado ajustes de design e função do slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Testimonial from "./Testimonial";
 import tanyaPic from "./assets/image-tanya.jpg";
 import chevronLeft from "./assets/icon-prev.svg";
 import chevronRight from "./assets/icon-next.svg";
-// import johnPic from "./assets/image-john.jpg";
+import johnPic from "./assets/image-john.jpg";
+import patternCurve from "./assets/pattern-curve.svg";
 
 function App() {
+  const [slide, setSlide] = useState(0);
+
   const data = [
     {
       name: "Tanya Sinclair",
@@ -14,23 +18,29 @@ function App() {
     I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
     excited about the future. ”`,
     },
-    // {
-    //   name: "John Tarkpor",
-    //   profession: "Junior Front-end Developer",
-    //   pic: johnPic,
-    //   text: `“ “ If you want to lay the best foundation possible I’d recommend taking this course.
-    // The depth the instructors go into is incredible. I now feel so confident about
-    // starting up as a professional developer. ”`,
-    // },
+    {
+      name: "John Tarkpor",
+      profession: "Junior Front-end Developer",
+      pic: johnPic,
+      text: `“ “ If you want to lay the best foundation possible I’d recommend taking this course.
+    The depth the instructors go into is incredible. I now feel so confident about
+    starting up as a professional developer. ”`,
+    },
   ];
 
   const Slider = () => {
     return (
       <div className="absolute -bottom-5 flex h-[38px] w-[80px] items-center  rounded-full bg-slate-50">
-        <button className="flex h-full w-1/2 items-center justify-center">
+        <button
+          className="flex h-full w-1/2 items-center justify-center"
+          onClick={() => setSlide(0)}
+        >
           <img src={chevronLeft} alt="Icon prev" className="h-4" />
         </button>
-        <button className="flex h-full w-1/2 items-center justify-center">
+        <button
+          className="flex h-full w-1/2 items-center justify-center"
+          onClick={() => setSlide(1)}
+        >
           <img src={chevronRight} alt="Icon next" className="h-4" />
         </button>
       </div>
@@ -38,16 +48,32 @@ function App() {
   };
 
   return (
-    <div>
-      {data.map((person) => (
+    <div className="relative h-screen">
+      {!slide ? (
         <Testimonial
-          key={person.name}
-          pic={person.pic}
-          text={person.text}
-          profession={person.profession}
+          key={data[0].name}
+          name={data[0].name}
+          pic={data[0].pic}
+          text={data[0].text}
+          profession={data[0].profession}
           slider={Slider()}
         />
-      ))}
+      ) : (
+        <Testimonial
+          key={data[1].name}
+          name={data[1].name}
+          pic={data[1].pic}
+          text={data[1].text}
+          profession={data[1].profession}
+          slider={Slider()}
+        />
+      )}
+
+      <img
+        src={patternCurve}
+        alt="Illustration"
+        className="absolute bottom-0 -z-10 w-full"
+      />
     </div>
   );
 }
--- src/Testimonial.jsx
+++ src/Testimonial.jsx
@@ -1,14 +1,16 @@
 import patternBg from "../src/assets/pattern-bg.svg";
+import quote from "../src/assets/pattern-quotes.svg";
 
 const Testimonial = (props) => {
   return (
     <div className="font-inter p-5">
-      <div className="relative mb-5">
+      <div className="relative mb-[54px]">
         <img
           src={patternBg}
           alt="Background"
           className="relative left-0 top-0"
         />
+
         <figcaption className="absolute top-1/2 flex -translate-y-1/2 justify-center">
           <img
             src={props.pic}
@@ -18,11 +20,21 @@ const Testimonial = (props) => {
           {props.slider}
         </figcaption>
       </div>
-      <div>
-        <figcaption>
+
+      <div className="relative">
+        <img
+          src={quote}
+          alt="Quote"
+          className="absolute -top-6 right-1/2 w-[60px] translate-x-1/2 "
+        />
+
+        <figcaption className="text-center text-lg text-[#202046]">
           <blockquote className="font-light ">{props.text}</blockquote>
-          <cite>
-            <span>{props.profession}</span>
+          <cite className="mt-5 flex flex-col font-bold not-italic">
+            {props.name}
+            <span className="font-medium text-[#babacf]">
+              {props.profession}
+            </span>
           </cite>
         </figcaption>
       </div>
